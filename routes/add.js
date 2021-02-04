700b1f6379a3ad55dec756e45f3640c0d06849ec
Fangzheng <fal044@ucsd.edu>
2021-02-04 10:44:23 -0800

    lab5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {
+  // Your code goes here
+  var name = request.query.name;
+  var des = request.query.description;
+  var newFriend = {
+    "name": name,
+    "description": des,
+    "imageURL": "http://lorempixel.com/500/500/people"
+  };
+  data.friends.push(newFriend);
+  response.render('index', data);
+}
